1672d4d7058e9058db641f8d78102658ad367b32
Rohan Pande <rohanpande@sanisoft.com>
2016-09-23 09:29:09 +0530

    added more changes

---
+++ webroot/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Blog</title>
+  </head>
+  <body>
+    <h1>My First Blogging System</h1>
+  </body>
+</html>
