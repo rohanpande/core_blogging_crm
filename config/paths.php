763d072c9efc5e560ff0f8bdc40c29a87f24c2f4
Rohan Pande <rohanpande@sanisoft.com>
2016-09-23 09:56:43 +0530

    removed vendor file

---
+++ config/paths.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('DS')) {
+  define('DS',DIRECTORY_SEPARATOR);
+}
+
+define('ROOT',dirname(__DIR__));
+
+define('APP_DIR','src');
+
+define('APP', ROOT . DS . APP_DIR . DS);
+
+define('CONFIG', ROOT . DS . 'config' . DS);
+
+define('WWW_ROOT', ROOT . DS . 'webroot' . DS);
--- webroot/index.php
+++ webroot/index.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First Blog</title>
-  </head>
-  <body>
-    <h1>My First Blogging System</h1>
-  </body>
-</html>
+
